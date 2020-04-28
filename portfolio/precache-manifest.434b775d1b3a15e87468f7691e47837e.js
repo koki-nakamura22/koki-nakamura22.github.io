8f806dd1992cd5ffa86a0095f3cafa6073ec4dab
Koki Nakamura <nk.levia22@gmail.com>
2020-04-28 22:59:26 +1000

    Updates

---
+++ portfolio/precache-manifest.434b775d1b3a15e87468f7691e47837e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f7e5dcb58bc7faa9e5f3350f8a74531",
+    "url": "/portfolio/static/media/self-face-photos.4f7e5dcb.jpg"
+  },
+  {
+    "revision": "63ba1d7af35dcceedc871c73af7ee783",
+    "url": "/portfolio/static/media/Resume_KokiNakamura.63ba1d7a.docx"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "ecba1a8cf576072b7586",
+    "url": "/portfolio/static/js/main.ecba1a8c.chunk.js"
+  },
+  {
+    "revision": "e65ff3ac8a3f04b012ed",
+    "url": "/portfolio/static/js/2.e65ff3ac.chunk.js"
+  },
+  {
+    "revision": "ecba1a8cf576072b7586",
+    "url": "/portfolio/static/css/main.7150cd36.chunk.css"
+  },
+  {
+    "revision": "e65ff3ac8a3f04b012ed",
+    "url": "/portfolio/static/css/2.daa5fdda.chunk.css"
+  },
+  {
+    "revision": "82fae4399a421ac5a33a5f236e58df28",
+    "url": "/portfolio/index.html"
+  }
+];
