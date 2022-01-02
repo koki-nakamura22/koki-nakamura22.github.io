0698973f85652fd540869ae424c461990a7b263b
Koki Nakamura <nk.levia22@gmail.com>
2022-01-02 17:07:55 +0900

    Updates

---
+++ portfolio/precache-manifest.fcd32ca1c6694ba8572f2e2e46b8e64b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "86285c95b733b744389e",
+    "url": "/portfolio/static/css/main.3d26360e.chunk.css"
+  },
+  {
+    "revision": "86285c95b733b744389e",
+    "url": "/portfolio/static/js/main.86285c95.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "ac1af6c8f8f6672111a4",
+    "url": "/portfolio/static/css/2.36222c2d.chunk.css"
+  },
+  {
+    "revision": "ac1af6c8f8f6672111a4",
+    "url": "/portfolio/static/js/2.ac1af6c8.chunk.js"
+  },
+  {
+    "revision": "4f7e5dcb58bc7faa9e5f3350f8a74531",
+    "url": "/portfolio/static/media/self-face-photos.4f7e5dcb.jpg"
+  },
+  {
+    "revision": "48067a402403b21ab7f35a123fcd129b",
+    "url": "/portfolio/static/media/Resume_KokiNakamura.48067a40.docx"
+  },
+  {
+    "revision": "eb774f6a213c8c9fae708e04b31cc754",
+    "url": "/portfolio/index.html"
+  }
+];
--- portfolio/service-worker.js
+++ portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.047b4382bb4e7751da765c36f5057922.js"
+  "/portfolio/precache-manifest.fcd32ca1c6694ba8572f2e2e46b8e64b.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
